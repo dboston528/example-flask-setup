4d6ea18edd4e065b24758d4510654bfc7f1340cd
DeAndre Boston <dboston3@yahoo.com>
2020-12-14 22:13:16 -0600

    Initial Commit

---
+++ flaskr/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+
+def create_app(test_config=None):
+
+    app = Flask(__name__)
+
+    @app.route('/')
+    def hello():
+        return jsonify({'message': 'Hello World'})
+
+    @app.route('/smile')
+    def smile():
+        return 'smile ;)'
+
+    return app
